bb63b14fa5adcb1923655f9d1cb3d1948167bff3
jefesfc <ldmruk@gmail.com>
2026-06-19 11:32:57 +0100

    dashboard: hide GettingStartedWidget for demo account, tour only

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,12 +1,18 @@
 import { PageTransition } from "../components/shared/PageTransition";
 import { HeroBanner } from "../components/dashboard/HeroBanner";
 import { GettingStartedWidget } from "../components/dashboard/GettingStartedWidget";
+import { useAuthStore } from "../store/auth-store";
+
+const DEMO_EMAIL = 'ldmrukuae@gmail.com';
 
 export default function DashboardPage() {
+  const user = useAuthStore((s) => s.user);
+  const isDemo = user?.email === DEMO_EMAIL;
+
   return (
     <PageTransition>
       <div className="p-6">
-        <GettingStartedWidget />
+        {!isDemo && <GettingStartedWidget />}
         <HeroBanner />
       </div>
     </PageTransition>
